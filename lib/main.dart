a42c4bbb82b7b6d624d306201500d479f95f9516
Bek <nematjanov95@gmail.com>
2022-11-07 22:55:20 +0500

    1 vazifa

--- lib/main.dart
+++ lib/main.dart
@@ -18,7 +18,7 @@ class MyApp extends StatelessWidget {
                 children: [
                   Container(
                     height: 500,
-                    width: 100,
+                    width: 50,
                     decoration: const BoxDecoration(
                         color: Color(0xFF01579B),
                         borderRadius: BorderRadius.only(
@@ -35,9 +35,13 @@ class MyApp extends StatelessWidget {
                     ),
                   ),
                   Container(
-                    width: 100,
                     height: 500,
-                    color: Color(0xFF0277BD),
+                    width: 50,
+                    decoration: const BoxDecoration(
+                        color: Color(0xFF2962FF),
+                        borderRadius: BorderRadius.only(
+                            bottomRight: Radius.circular(20),
+                            topRight: Radius.circular(20))),
                     child: const Center(
                       child: Text(
                         'U',
@@ -50,15 +54,110 @@ class MyApp extends StatelessWidget {
                   ),
                   Container(
                     height: 500,
-                    width: 100,
+                    width: 50,
                     decoration: const BoxDecoration(
                         color: Color(0xFF0288D1),
+                        borderRadius: BorderRadius.only(
+                            topLeft: Radius.circular(20),
+                            bottomLeft: Radius.circular(20))),
+                    child: const Center(
+                      child: Text(
+                        'X',
+                        style: TextStyle(
+                          decoration: TextDecoration.none,
+                          color: Colors.black,
+                        ),
+                        textAlign: TextAlign.center,
+                      ),
+                    ),
+                  ),
+                  Container(
+                    height: 500,
+                    width: 50,
+                    decoration: const BoxDecoration(
+                        color: Color(0xFF039BE5),
                         borderRadius: BorderRadius.only(
                             topRight: Radius.circular(20),
                             bottomRight: Radius.circular(20))),
                     child: const Center(
                       child: Text(
-                        'X',
+                        'A',
+                        style: TextStyle(
+                          decoration: TextDecoration.none,
+                          color: Colors.black,
+                        ),
+                        textAlign: TextAlign.center,
+                      ),
+                    ),
+                  ),
+                  Container(
+                    height: 500,
+                    width: 50,
+                    decoration: const BoxDecoration(
+                        color: Color(0xFF29B6F6),
+                        borderRadius: BorderRadius.only(
+                            topLeft: Radius.circular(20),
+                            bottomLeft: Radius.circular(20))),
+                    child: const Center(
+                      child: Text(
+                        'M',
+                        style: TextStyle(
+                          decoration: TextDecoration.none,
+                          color: Colors.black,
+                        ),
+                        textAlign: TextAlign.center,
+                      ),
+                    ),
+                  ),
+                  Container(
+                    height: 500,
+                    width: 50,
+                    decoration: const BoxDecoration(
+                        color: Color(0xFF4FC3F7),
+                        borderRadius: BorderRadius.only(
+                            topRight: Radius.circular(20),
+                            bottomRight: Radius.circular(20))),
+                    child: const Center(
+                      child: Text(
+                        'M',
+                        style: TextStyle(
+                          decoration: TextDecoration.none,
+                          color: Colors.black,
+                        ),
+                        textAlign: TextAlign.center,
+                      ),
+                    ),
+                  ),
+                  Container(
+                    height: 500,
+                    width: 50,
+                    decoration: const BoxDecoration(
+                        color: Color(0xFF81D4FA),
+                        borderRadius: BorderRadius.only(
+                            topLeft: Radius.circular(20),
+                            bottomLeft: Radius.circular(20))),
+                    child: const Center(
+                      child: Text(
+                        'A',
+                        style: TextStyle(
+                          decoration: TextDecoration.none,
+                          color: Colors.black,
+                        ),
+                        textAlign: TextAlign.center,
+                      ),
+                    ),
+                  ),
+                  Container(
+                    height: 500,
+                    width: 50,
+                    decoration: const BoxDecoration(
+                        color: Color(0xFFB3E5FC),
+                        borderRadius: BorderRadius.only(
+                            topRight: Radius.circular(20),
+                            bottomRight: Radius.circular(20))),
+                    child: const Center(
+                      child: Text(
+                        'D',
                         style: TextStyle(
                           decoration: TextDecoration.none,
                           color: Colors.black,
