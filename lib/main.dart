bc62f3df73ca3e78240c52cfad2476bf25eec88f
Bek <nematjanov95@gmail.com>
2022-11-09 03:45:47 +0500

    update

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
-import '1-vazifa.dart';
-import '2-vazifa.dart';
-import '3-vazifa.dart';
-import '4-vazifa.dart';
-import '5-vazifa.dart';
+import '9 November_Stack/1-vazifa.dart';
+import '9 November_Stack/2-vazifa.dart';
+import '9 November_Stack/3-vazifa.dart';
+import '9 November_Stack/4-vazifa.dart';
+import '9 November_Stack/5-vazifa.dart';
 
 void main() {
   runApp(const FifthdWork());
