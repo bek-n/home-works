6bfcd383405e817e90a1685c4948c30b2c36b4d6
Bek <nematjanov95@gmail.com>
2022-11-10 08:01:39 +0500

    2 vazifa

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
 
 import '9-November, margin,Shadow/1-vazifa.dart';
+import '9-November, margin,Shadow/2-vazifa.dart';
 
 void main() {
-  runApp(const First());
+  runApp(const Second());
 }
