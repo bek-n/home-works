bd62b87a40c514d547277f038cabf8d705a2f150
Bek <nematjanov95@gmail.com>
2022-11-15 23:07:56 +0500

    menu dark mode

--- lib/main.dart
+++ lib/main.dart
@@ -5,7 +5,7 @@ import 'package:flutter/material.dart';
 import '15 November. Scroll/ligh_mode menu.dart';
 import '15 November. Scroll/light mode.dart';
 import '15 November. Scroll/night mode.dart';
-import 'menu_night mode.dart';
+import '15 November. Scroll/menu_night mode.dart';
 
 void main() {
   runApp(const menuDarkode());
