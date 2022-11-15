a2ab8610c333f00c22217af0fce9d50db53d3b16
Bek <nematjanov95@gmail.com>
2022-11-15 22:49:38 +0500

    main dark mode

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,8 @@ import 'package:flutter/material.dart';
 
 import '15 November. Scroll/ligh_mode menu.dart';
 import '15 November. Scroll/light mode.dart';
+import '15 November. Scroll/night mode.dart';
 
 void main() {
-  runApp(const menuLightMode());
+  runApp(const nightMode());
 }
