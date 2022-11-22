0ebfa7591e9c0993aa83f4d98e57592137afcdaf
Bek <nematjanov95@gmail.com>
2022-11-22 22:47:32 +0500

    Statefull

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
-import '21 November/dark mode/dark mode/1st page.dart';
+
+import '22 November. /2nd page.dart';
 
 
 
@@ -9,21 +10,19 @@ import '21 November/dark mode/dark mode/1st page.dart';
 
 
 
-// import '17 November Wrap, Grid view/1.dart';
-// import '17 November Wrap, Grid view/2.dart';
-// import '17 November. Alisher nanoiy page to page/2nd page.dart';
+
 
 void main() {
-  runApp(const  firstt());
+  runApp(const  MyApp());
 }
 
-// class MyApp extends StatelessWidget {
-//   const MyApp({super.key});
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
-//   @override
-//   Widget build(BuildContext context) {
-//     return MaterialApp(
-//       home: classs(),
-//     );
-//   }
-// }
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: secondPage(),
+    );
+  }
+}
