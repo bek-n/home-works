3f3dd79aae4a37d7241f71512433a4c43e174ee6
Bek <nematjanov95@gmail.com>
2022-11-24 23:54:00 +0500

    likes count

--- lib/23 November/instalike.dart
+++ lib/23 November/instalike.dart
@@ -12,50 +12,68 @@ class homework extends StatefulWidget {
 
 List<dataOfUsers> datas = [
   dataOfUsers(
+    comments: '(130)',
+      numsOfLike: 200,
       name: 'Christina Smith',
       avatar:
           'https://afroculture.net/wp-content/uploads/2019/01/GabrielleUnion1.jpg',
       media:
           'https://cdn.getyourguide.com/img/tour/62a67900040cb.jpeg/145.jpg'),
   dataOfUsers(
+    comments: '(12330)',
+      numsOfLike: 1300,
       name: 'Joe Evans',
       avatar:
           'https://www.mrdustbin.com/us/wp-content/uploads/2021/08/kevin-hart.jpg',
       media:
           'https://res.cloudinary.com/thrillophilia/image/upload/c_fill,f_auto,fl_progressive.strip_profile,g_auto,q_auto/v1/filestore/35djn21pcljb4zmfrhhqggpztpld_1562050063_z.jpg'),
   dataOfUsers(
+    comments: '(340)',
+      numsOfLike: 50,
       name: 'Linda Joe',
       avatar:
           'https://cdn.fansshare.com/pictures/famousactresses/famous-american-actress-emma-watson-bikini-2122545680.jpg',
       media:
           'https://phonoteka.org/uploads/posts/2021-07/1625611051_33-phonoteka-org-p-garri-potter-ron-i-germiona-art-krasivo-33.jpg'),
   dataOfUsers(
+    comments: '(8450)',
+      numsOfLike: 430,
       name: 'Elizabeth Olsen',
       avatar:
           'https://4kwallpapers.com/images/wallpapers/elizabeth-olsen-portrait-american-actress-3840x2160-4794.jpg',
       media:
           'https://tusresiduos.com/wp-content/uploads/2022/10/Elizabeth-Olsen-se-enfrenta-a-las-peliculas-de-Marvel-son.jpg'),
   dataOfUsers(
+    comments: '(321)',
+      numsOfLike: 5340,
       name: 'Salman Khan',
       avatar: 'https://theindianeye.com/wp-content/uploads/2022/01/Two.jpeg',
       media: 'https://i.ytimg.com/vi/3ldClowVz-g/maxresdefault.jpg'),
   dataOfUsers(
+    comments: '(523)',
+      numsOfLike: 5430,
       name: 'The Rock',
       avatar:
           'https://media1.popsugar-assets.com/files/thumbor/nza_2rG2FUqd9MWHxb6ok-Um800/fit-in/2048xorig/filters:format_auto-!!-:strip_icc-!!-/2018/08/23/676/n/1922283/4a13185c87b19384_GettyImages-470569924/i/Dwayne-Johnson.jpg',
       media: 'https://www.blackadammovie.net/images/share.jpg'),
   dataOfUsers(
+    comments: '(400)',
+      numsOfLike: 2300,
       name: 'Sandra Ullock',
       avatar:
           'https://www.emdria.org/wp-content/uploads/2021/12/Sandra-Bullock-Shutterstock-scaled.jpg',
       media:
           'https://www.stardust.it/uploads/img/post/77c06c680a9d6030cb5cc63eec82f4c81650986810.jpeg'),
   dataOfUsers(
+    comments: '(135)',
+      numsOfLike: 2500,
       name: 'Jane Foster',
       avatar: 'https://nettv4u.com/imagine/19-01-2021/leah-pipes.jpg',
       media:
           'https://www.welikela.com/wp-content/uploads/2022/11/Arboretum-Lightscape-2022.jpg'),
   dataOfUsers(
+    comments: '(333)',
+      numsOfLike: 13420,
       name: 'Michae B.Jordan',
       avatar:
           'https://i.pinimg.com/474x/99/63/c1/9963c1d185510386cdc5b7f6da07f14d.jpg',
@@ -224,17 +242,34 @@ class _homeworkState extends State<homework> {
                           child: Row(
                             children: [
                               Padding(
-                                padding: const EdgeInsets.only(left: 18),
-                                child: Image.asset(
-                                  'assets/Heart.png',
-                                  width: 20,
-                                  height: 19,
-                                ),
-                              ),
+                                  padding: const EdgeInsets.only(left: 18),
+                                  child: datas[index].isLike
+                                      ? GestureDetector(
+                                          onTap: () {
+                                            datas[index].isLike =
+                                                !datas[index].isLike;
+                                            datas[index].numsOfLike -= 1;
+                                            setState(() {});
+                                          },
+                                          child: Image.asset(
+                                            'assets/Heart.png',
+                                            width: 20,
+                                            height: 19,
+                                          ),
+                                        )
+                                      : GestureDetector(
+                                          onTap: () {
+                                            datas[index].isLike =
+                                                !datas[index].isLike;
+                                                 datas[index].numsOfLike += 1;
+                                            setState(() {});
+                                          },
+                                          child: Icon(Icons.favorite),
+                                        )),
                               Padding(
                                 padding: const EdgeInsets.only(left: 6),
                                 child: Text(
-                                  '189',
+                                  datas[index].numsOfLike.toString(),
                                   style: TextStyle(fontSize: 16),
                                 ),
                               ),
@@ -289,7 +324,7 @@ class _homeworkState extends State<homework> {
                                 ),
                               ),
                               Text(
-                                '(32)',
+                                datas[index].comments,
                                 style:
                                     TextStyle(color: Colors.grey, fontSize: 15),
                               ),
--- lib/23 November/users data.dart
+++ lib/23 November/users data.dart
@@ -1,5 +1,6 @@
 class dataOfUsers {
-  final String name, avatar, media;
-
-  dataOfUsers({required this.name, required this.avatar, required this.media});
+  final String name, avatar, media,comments;
+   int numsOfLike;
+  bool isLike = false;
+  dataOfUsers({required this.name, required this.avatar, required this.media,required this.numsOfLike,required this.comments});
 }
